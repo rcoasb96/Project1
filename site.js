363407b7e6187381840a98ac4263a94469b79540
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2017-03-02 23:31:14 -0600

    Changed alert to appear if no fields have input and submit button is clicked

--- site.js
+++ site.js
@@ -4,7 +4,11 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
   
-  $('form').submit(function(e){
-    alert('Puppies are going your way!');
+  $('#form').submit(function(e) {
+    if($.trim($('#email').val()) === '' || $.trim($('#name').val()) === '') {
+      alert('Oops! Make sure you filled everything!');
+      return false;
+    }
+    event.preventDefault();
   });
 });
